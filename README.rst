187663b4cfdb6c9e58c74ad06799154df7195210
Amruth VVKP <amruthvvkp@gmail.com>
2020-07-12 22:52:32 +0530

    Created a base skeleton UI and API working model

--- README.rst
+++ README.rst
@@ -7,4 +7,4 @@ This simple project is an example repo for Python projects.
 
 ---------------
 
-If you want to learn more about ``setup.py`` files, check out `this repository <https://github.com/amruthvvkp/stock_screener_fastapi/setup.py>`_.
+If you want to learn more about ``setup.py`` files, check out `this repository <https://github.com/amruthvvkp/stock_screener_fastapi/blob/master/setup.py>`_.
--- model/main.py
+++ model/main.py
@@ -1,14 +1,35 @@
 """Fast API Entry Point"""
 
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello":"World"}
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+
+@app.get("/")
+def home(request: Request):
+    """
+    Dashboard/Home page
+    :return: JSON response for homepage
+    :rtype: dict
+    """
+    return templates.TemplateResponse("home.html", {
+        "request": request,
+        "somevar": 2
+    })
+
+
+@app.post("/stock")
+def create_stock():
+    """
+    Creates stocks and stores it in the database
+    :return: JSON response post creation
+    :rtype: dict
+    """
+    return {
+        "code": "success",
+        "message": "stock created"
+    }
